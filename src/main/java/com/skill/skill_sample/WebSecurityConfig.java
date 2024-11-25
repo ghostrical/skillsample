8dcecfd2a49d172f3f1d078a89a732140f513b8b
ghostrical <shinwder@naver.com>
2024-11-25 21:26:38 +0900

    20241125 나갔다옴

---
+++ src/main/java/com/skill/skill_sample/WebSecurityConfig.java
@@ -0,0 +1,10 @@
+package com.skill.skill_sample;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig {
+}
